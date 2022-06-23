0d0b71e124ca9279bb3401d8402a9f88a9593483
ZanderKong <1791348990@qq.com>
2022-06-23 15:23:42 +0800

    docker commit

---
+++ src/main/java/com/zandertape/tapemusic/TapeMusicApplication.java
@@ -0,0 +1,13 @@
+package com.zandertape.tapemusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TapeMusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TapeMusicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zandertape/tapemusic/TapeMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zandertape.tapemusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TapeMusicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
